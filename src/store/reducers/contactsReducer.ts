fb7cdf0ca8f868cbcacca9d20fb419572b388dea
Mertcan Sahin <mertcan.sahin44@gmail.com>
2024-11-16 22:31:54 +1100

    contacts list updated

--- src/store/reducers/contactsReducer.ts
+++ src/store/reducers/contactsReducer.ts
@@ -17,8 +17,65 @@ export type IContactStore = {
     nextId: number
 }
 const initialState: IContactStore = {
-    contacts: [],
-    nextId: 1,
+    contacts: [
+        {
+            id: 1,
+            name: "John Smith",
+            phoneNumber: "0299882211",
+            department: "Information Communications Technology",
+            address: "1 Code Lane",
+            city: "Javaville",
+            state: "NSW",
+            zipCode: "0100",
+            country: "Australia",
+        },
+        {
+            id: 2,
+            name: "Sue White",
+            phoneNumber: "0388992255",
+            department: "Finance",
+            address: "16 Bit Way",
+            city: "Byte Cove",
+            state: "QLD",
+            zipCode: "1101",
+            country: "Australia",
+        },
+        {
+            id: 3,
+            name: "Bob O’Bits",
+            phoneNumber: "0577882255",
+            department: "Marketing",
+            address: "8 Silicon Road",
+            city: "Cloud Hills",
+            state: "VIC",
+            zipCode: "1001",
+            country: "Australia",
+        },
+        {
+            id: 4,
+            name: "Mary Blue",
+            phoneNumber: "0644559988",
+            department: "Finance",
+            address: "4 Processor Boulevard",
+            city: "Appletson",
+            state: "NT",
+            zipCode: "1010",
+            country: "Australia",
+        },
+
+        {
+            id: 5,
+            name: "Nick Green",
+            phoneNumber: "0299881122",
+            department: "Marketing",
+            address: "700 Bandwidth Street",
+            city: "Bufferland",
+            state: "NSW",
+            zipCode: "0110",
+            country: "Australia",
+        },
+    ],
+    nextId: 6,
 }
 const contactsReducer = createSlice({
     name: 'contactsReducer',
